8e02ab5e992cc00d768549bd00be2750be18336c
barisdundar <baris_809@windowslive.com>
2022-02-28 01:00:18 +0300

    Ortaksi
    Ortaksi uygulaması, benzer yönlere gidecek birbirinden farklı yolcuların, benzer konumda birbirleri ile eşleşmesi sonucu ortaklaşa taksi kullanmalarını sağlayan bir mobil uygulamadır

---
+++ ortaksi/index.js
@@ -0,0 +1,7 @@
+// uygulama buradan başlıyor
+// react native ile hazır geliyor bu kısımlar
+
+import { AppRegistry } from 'react-native';
+import App from './src/App'
+
+AppRegistry.registerComponent('ortaksi', () => App);
